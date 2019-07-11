fc82b1a98469580759bb25134aad8bdd71fb29c7
MariusSimion79 <mariussmn79@gmail.com>
2019-07-11 20:07:44 +0300

    Version1

---
+++ ZOO/ZOO/Animal.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZOO
+{
+    public enum FavoriteFood { Grass, Carrots, Meat, Milk, Bananas, Fish, Branches, Corn }
+    public class Animal
+    {
+        private string animalName;
+        private string species;
+        private FavoriteFood favoriteFood;
+        public Zoo CurrentZoo { get; set; }
+
+        public string AnimalName
+        {
+            get { return animalName; }
+            private set { this.animalName = value; }      //Animals can`t change name
+        }
+        public string Species
+        {
+            get { return species; }
+            private set { this.species = value; }     //Animal can`t change species
+        }
+        public FavoriteFood FavoriteMeal
+        {
+            get { return favoriteFood; }
+            set { this.favoriteFood = value; }          //Animal can change favorite food
+        }
+
+
+
+
+        public Animal(string species, string animalName, FavoriteFood favoriteFood/*, Zoo currentZoo*/)
+        {
+            this.AnimalName = animalName;
+            this.Species = species;
+            this.FavoriteMeal = favoriteFood;
+            // this.CurrentZoo = currentZoo;
+            //currentZoo.animalList.Add(animalName);
+
+        }
+
+        public void Eat()
+        {
+            Console.WriteLine($"{this.species} {this.animalName} eat {this.favoriteFood}");
+        }
+
+
+    }
+}
---
+++ ZOO/ZOO/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace ZOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Zoo creation
+            Zoo zoo1 = new Zoo("Vien Zoo");
+            Zoo zoo2 = new Zoo("New York Zoo");
+
+            //Zoo Renaming
+            zoo2.zooName = "N.Y. Zoo";
+
+            //Animal creation
+            Animal animal1 = new Animal("Dumbo", "Elephant", FavoriteFood.Grass);
+            zoo1.BringAnimal(animal1, zoo1);
+            Animal animal2 = new Animal("King Kong", "Gorilla", FavoriteFood.Bananas);
+            zoo1.BringAnimal(animal2, zoo1);
+            Animal animal3 = new Animal("Leo", "Lion", FavoriteFood.Meat);
+            zoo1.BringAnimal(animal3, zoo1);
+            Animal animal4 = new Animal("Tom", "Cat", FavoriteFood.Milk);
+            zoo1.BringAnimal(animal4, zoo1);
+            Animal animal5 = new Animal("Dundee", "Crocodile", FavoriteFood.Meat);
+            zoo1.BringAnimal(animal5, zoo1);
+            Animal animal6 = new Animal("Chewbacca", "Gorilla", FavoriteFood.Meat);
+            zoo1.BringAnimal(animal6, zoo1);
+            Animal animal7 = new Animal("Vegan Leo", "Lion", FavoriteFood.Grass);
+            zoo1.BringAnimal(animal7, zoo1);
+            Animal animal8 = new Animal("Baloo", "Bear", FavoriteFood.Fish);
+            zoo2.BringAnimal(animal8, zoo2);
+            Animal animal9 = new Animal("Kaa", "Snake", FavoriteFood.Meat);
+            zoo2.BringAnimal(animal9, zoo2);
+            Animal animal10 = new Animal("Melman", "Giraffe", FavoriteFood.Branches);
+            zoo2.BringAnimal(animal10, zoo2);
+            Animal animal11 = new Animal("Alex", "Zebra", FavoriteFood.Grass);
+            zoo2.BringAnimal(animal11, zoo2);
+
+            Console.WriteLine("Next, you`ll se what each animal love to eat.");
+            animal1.Eat();
+            animal2.Eat();
+            animal3.Eat();
+            animal4.Eat();
+            animal5.Eat();
+            animal6.Eat();
+            animal7.Eat();
+            animal8.Eat();
+            animal9.Eat();
+            animal10.Eat();
+            animal11.Eat();
+
+            //animal1.AnimalName = "blabla";      //  Animal can`t change name.
+            //animal1.Species = "blabla";         // Animal can`t change name.
+
+            Console.WriteLine("-----------------------------------------------------------------------------------------------");
+
+            zoo1.FeedAnimal();
+
+            Console.WriteLine("-----------------------------------------------------------------------------------------------");
+
+            zoo1.ZooInfo();
+
+            Console.WriteLine("-----------------------------------------------------------------------------------------------");
+
+            zoo2.ZooInfo();
+
+            Console.WriteLine("-----------------------------------------------------------------------------------------------");
+
+            // Transfer animals between Zoo`s
+            zoo1.TransferAnimal(animal1, zoo2);
+            zoo2.TransferAnimal(animal11, zoo1);
+
+            Console.WriteLine("-----------------------------------------------------------------------------------------------");
+
+            zoo1.ZooInfo();
+
+            Console.WriteLine("-----------------------------------------------------------------------------------------------");
+
+            zoo2.ZooInfo();
+
+            Console.WriteLine("-----------------------------------------------------------------------------------------------");
+
+        }
+    }
+}
